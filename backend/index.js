9fb07d2bb1b45de3399023eb180ae014933f51fe
Youness <y.ouzzedi@hotmail.com>
2023-10-03 15:32:49 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        {headers: {"private-key": "eb9c8a3a-05f4-4d56-894f-57fa64576a0f"}}
+    )
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
+});
+
+app.listen(3001);
