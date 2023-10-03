8ebd8b2b14a36e342e83c22350299bdd8fba79f8
Youness <y.ouzzedi@hotmail.com>
2023-10-03 15:50:45 +0200

    Add react-chat-engine-advanced

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,17 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        '5d81f7d9-da3d-47a1-a09a-480bf742e96f',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }}/>
+        </div>
+    )
 }
 
 export default ChatsPage
